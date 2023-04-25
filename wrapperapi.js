d269b11655e31e6af17bd0f294c3426014181176
palabhavishruti <127177352+palabhavishruti@users.noreply.github.com>
2023-04-25 10:30:32 +0530

    Add files via upload

---
+++ wrapperapi.js
@@ -0,0 +1,101 @@
+var request = require('request');
+var express = require('express')
+var app = express();
+var port = 3101
+app.use(express.json())
+
+var druidUrl = 'http://localhost:8888/druid/indexer/v1/supervisor';
+var sqlqueryUrl = 'http://localhost:8888/druid/v2/sql'
+var nativequeryUrl = 'http://localhost:8888/druid/v2/'
+
+app.post('/create', (req, res) => {
+  try {
+    var ingestionSpec = req.body
+    request.post({
+      url: druidUrl,
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(ingestionSpec)
+    }, function (error, response) {
+      try {
+        if (response.statusCode !== 200) {
+          res.status(400).send("not a proper request")
+        } else {
+          res.status(200).send(response)
+        }
+      }
+      catch {
+        res.send(error)
+      }
+    }
+    )
+  }
+  catch (error) {
+    res.status(500).send("not able to handle the request")
+  }
+})
+
+
+app.post('/nativequery', (req, res) => {
+  try {
+    var nquery = req.body
+    request.post({
+      url: nativequeryUrl,
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(nquery)
+      
+    }, function (error, response,body) {
+      try {
+        if (response.statusCode !== 200) {
+          res.status(400).json({ "errorMessage": "its not a proper request body" })
+        } else {
+          const parsedBody = JSON.parse(body);
+    
+          res.status(200).send(parsedBody)
+        }
+      }
+      catch {
+        res.send(error)
+      }
+    }
+    )
+  }
+  catch (error) {
+    res.status(500).json({ "errorMessage": "unable to handle the request" })
+  }
+})
+
+app.post('/sqlquery', (req, res) => {
+  try {
+    var query = req.body
+    request.post({
+      url: sqlqueryUrl,
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(query)
+      
+    }, function (error, response,body) {
+      try {
+        if (response.statusCode !== 200) {
+          res.status(400).json({ "errorMessage": "its not a proper request body" })
+        } else {
+          const parsedBody = JSON.parse(body);
+    
+          res.status(200).send(parsedBody)
+        }
+      }
+      catch {
+        res.send(error)
+      }
+    }
+    )
+  }
+  catch (error) {
+    res.status(500).json({ "errorMessage": "unable to handle the request" })
+  }
+})
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
+
+module.exports = app
